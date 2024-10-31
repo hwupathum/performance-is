87ca5e3e94cd0f3733944530ddcb752c24a11a96
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-10-31 17:46:20 +0530

    Update concurrency levels

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -220,7 +220,7 @@ if [ "$concurrency" == "50-500" ]; then
     default_concurrent_users="50 100 150 300 500"
 elif [ "$concurrency" == "500-3000" ]; then
     echo "Running tests for concurrency level 500-3000"
-    default_concurrent_users="500 1000 1500 2000 2500 3000"
+    default_concurrent_users="500 1500 3000"
 elif [ "$concurrency" == "1000-3000" ]; then
     echo "Running tests for concurrency level 1000-3000"
     default_concurrent_users="1000 1500 2000 2500 3000"

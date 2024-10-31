c53a20e6fa425c9d343d209ed212eed7c11b4659
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-10-31 11:51:31 +0530

    Use 100k snapshot

--- common/deployment/setup/resources/createDB.sql
+++ common/deployment/setup/resources/createDB.sql
@@ -1,11 +1,9 @@
+drop database if exists IDENTITY_DB;
+
 create database IDENTITY_DB;
-create database UM_DB;
-create database REG_DB;
 
 use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
 use IDENTITY_DB; source ~/wso2is/dbscripts/consent/mysql.sql;
-use UM_DB; source ~/wso2is/dbscripts/mysql.sql;
-use REG_DB; source ~/wso2is/dbscripts/mysql.sql;
 
 -- add tables that could vary in different IS packs
 use IDENTITY_DB;
